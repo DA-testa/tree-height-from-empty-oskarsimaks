c75fe2e23e23a7905c9fe19dc0fd90d5bd659fb4
Oskars Imaks 221RDB462 <oskars.imaks@edu.rtu.lv>
2023-03-05 19:39:25 +0200

    M2

--- tree_height.py
+++ tree_height.py
@@ -5,54 +5,41 @@ import threading
 import numpy
 
 
-def compute_height(sakne, liste):
+def compute_height(n, parents):
     # Write this function
+    max_height = 0
     # Your code here
-    if not liste[sakne]:
-        return 1
-    else:
-        return 1 + max ([compute_height(berns, liste) for berns in liste[sakne]])
-    
+    arr = parents
+    # print(arr[0])
+    for i in range(n):
+        if int(arr[i]) != -1:
+            print(arr[i])
 
-#1
+    return max_height
+
+
 def main():
     # implement input form keyboard and from files
-    decis = input()
-    #print()
+    decis = input("Input F or I \n")
+    print()
     if decis == "F":
-        file= input()
-        if "a" in file:
-            return
-        with open(f"./test/{file}", mode="r") as file:
-            n=int(file.readline())
-            parents = list(map(int, file.readline().split(" ")))
-        liste = [[] for i in range(n)]
-        for i in range(n):
-            if int(parents[i]) == -1:
-                sakne = i
-            else:
-                liste[parents[i]].append(i)
-
-        print(compute_height(sakne, liste))
-        
+        # file= open("./test/F0")
+        print("File")
+        # mismatch = find_mismatch(file)
+        # file.close()
         
     elif decis == "I":
         n = int(input("Input n: "))
         parents = input("Input parents: ")
 
-        split_parents = list(map(int, parents.split(" ")))
-        
-        liste = [[] for i in range(n)]
-        for i in range(n):
-            if split_parents[i] == -1:
-                sakne = i
-            else:
-                liste[split_parents[i]].append(i)
+        split_parents = parents.split(" ")
+        split_parents = list(map(int, split_parents))
 
-        print(compute_height(sakne, liste))
+        rez = compute_height(n, split_parents)
     
     else:
-        return
+        print("Unknown command")
+    print()
 
     # Printing answer, write your code here'
     # let user input file name to use, don't allow file names with letter a
@@ -61,13 +48,13 @@ def main():
     # input number of elements
     # input values in one variable, separate with space, split these values in an array
     # call the function and output it's result
-    #pass
+    pass
 
 # In Python, the default limit on recursion depth is rather low,
 # so raise it here for this problem. Note that to take advantage
 # of bigger stack, we have to launch the computation in a new thread.
-# sys.setrecursionlimit(10**7)  # max depth of recursion
-# threading.stack_size(2**27)   # new thread will get stack of such size
-# threading.Thread(target=main).start()
+sys.setrecursionlimit(10**7)  # max depth of recursion
+threading.stack_size(2**27)   # new thread will get stack of such size
+threading.Thread(target=main).start()
 # main()
 # print(numpy.array([1,2,3]))
