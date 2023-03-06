d3fc4bf7a38f36456294370bbcb04a0836eced39
Oskars Imaks 221RDB462 <oskars.imaks@edu.rtu.lv>
2023-03-06 08:15:11 +0200

    KAKA

--- tree_height.py
+++ tree_height.py
@@ -18,7 +18,7 @@ def compute_height(sakne, liste):
 def main():
     
     # implement input form keyboard and from files
-    TEST= input()
+    # TESTS_IR_STULBS = input()
     decis = input()
     # print()
     if decis == "F":
